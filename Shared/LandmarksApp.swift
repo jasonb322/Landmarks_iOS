cdf7a6c29ade57f35fee50e586796a70774945e5
jasonb322 <bahrcreative@gmail.com>
2021-11-28 09:17:47 -0500

    Initial Commit

---
+++ Shared/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Shared
+//
+//  Created by Jason Bahr on 11/28/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
